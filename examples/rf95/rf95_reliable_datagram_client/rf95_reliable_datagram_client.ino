e29b8db43fcee328831fad9ae4d9cbf7bd08ec35
luisgcu <angelgcu@gmail.com>
2020-03-20 22:27:26 -0400

    Rf95/96 example edit

--- examples/rf95/rf95_reliable_datagram_client/rf95_reliable_datagram_client.ino
+++ examples/rf95/rf95_reliable_datagram_client/rf95_reliable_datagram_client.ino
@@ -1,9 +1,25 @@
-// rf95_reliable_datagram_server.pde
+// rf95_reliable_datagram_client.pde
 // -*- mode: C++ -*-
-// Example sketch showing how to create a simple addressed, reliable messaging server
+// Example sketch showing how to create a simple addressed, reliable messaging client
 // with the RHReliableDatagram class, using the RH_RF95 driver to control a RF95 radio.
-// It is designed to work with the other example rf95_reliable_datagram_client
+// It is designed to work with the other example rf95_reliable_datagram_server
 // Tested with Anarduino MiniWirelessLoRa, Rocket Scream Mini Ultra Pro with the RFM95W 
+/* Arduino Nano Pins
+ * | RFM95/96|Arduino Nano|
+ * |  NSS    |D10         |
+ * |  SCK    |D13         |
+ * |  MISO   |D12         |
+ * |  MOSI   |D11         |
+ * |  DIO0   |D2          |
+ * |----------------------|
+ * |----------------------|
+ * | RFM95/96|ESP32       |
+ * |  NSS    |GPIO05      |
+ * |  SCK    |GPIO18      |
+ * |  MISO   |GPIO19      |
+ * |  MOSI   |GPIO23      |
+ * |  DIO0   |GPIO15      
+*/
 
 #include <RHReliableDatagram.h>
 #include <RH_RF95.h>
@@ -13,11 +29,12 @@
 #define SERVER_ADDRESS 2
 
 // Singleton instance of the radio driver
-RH_RF95 driver;
-//RH_RF95 driver(5, 2); // Rocket Scream Mini Ultra Pro with the RFM95W
+RH_RF95 driver(10,2);    //Arduino nano
+//RH_RF95 driver(5, 15); //ESP32---> (RFM95_CS, RFM95_INT)
+//RH_RF95 driver(4, 2);  // Moteino Mega
 
 // Class to manage message delivery and receipt, using the driver declared above
-RHReliableDatagram manager(driver, SERVER_ADDRESS);
+RHReliableDatagram manager(driver, CLIENT_ADDRESS);
 
 // Need this on Arduino Zero with SerialUSB port (eg RocketScream Mini Ultra Pro)
 //#define Serial SerialUSB
@@ -29,7 +46,7 @@ void setup()
 //  pinMode(4, OUTPUT);
 //  digitalWrite(4, HIGH);
 
-  Serial.begin(9600);
+  Serial.begin(115200);
   while (!Serial) ; // Wait for serial port to be available
   if (!manager.init())
     Serial.println("init failed");
@@ -38,7 +55,8 @@ void setup()
   // The default transmitter power is 13dBm, using PA_BOOST.
   // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
   // you can set transmitter powers from 5 to 23 dBm:
-//  driver.setTxPower(23, false);
+ driver.setTxPower(5, false);
+  driver.setFrequency(433.0);
   // If you are using Modtronix inAir4 or inAir9,or any other module which uses the
   // transmitter RFO pins and not the PA_BOOST pins
   // then you can configure the power transmitter power for -1 to 14 dBm and with useRFO true. 
@@ -50,28 +68,34 @@ void setup()
 //  driver.setCADTimeout(10000);
 }
 
-uint8_t data[] = "And hello back to you";
+uint8_t data[] = "Hello World!";
 // Dont put this on the stack:
 uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
 
 void loop()
 {
-  if (manager.available())
+  Serial.println("Sending to rf95_reliable_datagram_server");
+    
+  // Send a message to manager_server
+  if (manager.sendtoWait(data, sizeof(data), SERVER_ADDRESS))
   {
-    // Wait for a message addressed to us from the client
+    // Now wait for a reply from the server
     uint8_t len = sizeof(buf);
-    uint8_t from;
-    if (manager.recvfromAck(buf, &len, &from))
+    uint8_t from;   
+    if (manager.recvfromAckTimeout(buf, &len, 2000, &from))
     {
-      Serial.print("got request from : 0x");
+      Serial.print("got reply from : 0x");
       Serial.print(from, HEX);
       Serial.print(": ");
       Serial.println((char*)buf);
-
-      // Send a reply back to the originator client
-      if (!manager.sendtoWait(data, sizeof(data), from))
-        Serial.println("sendtoWait failed");
+    }
+    else
+    {
+      Serial.println("No reply, is rf95_reliable_datagram_server running?");
     }
   }
+  else
+    Serial.println("sendtoWait failed");
+  delay(500);
 }
 
--- examples/rf95/rf95_reliable_datagram_server/rf95_reliable_datagram_server.ino
+++ examples/rf95/rf95_reliable_datagram_server/rf95_reliable_datagram_server.ino
@@ -1,9 +1,28 @@
-// rf95_reliable_datagram_client.pde
+// rf95_reliable_datagram_server.pde
 // -*- mode: C++ -*-
-// Example sketch showing how to create a simple addressed, reliable messaging client
+// Example sketch showing how to create a simple addressed, reliable messaging server
 // with the RHReliableDatagram class, using the RH_RF95 driver to control a RF95 radio.
-// It is designed to work with the other example rf95_reliable_datagram_server
+// It is designed to work with the other example rf95_reliable_datagram_client
 // Tested with Anarduino MiniWirelessLoRa, Rocket Scream Mini Ultra Pro with the RFM95W 
+
+/*    Arduino Nano Pins
+ * |----------------------|
+ * | RFM95/96|Arduino Nano|
+ * |----------------------|
+ * |  NSS    |D10         |
+ * |  SCK    |D13         |
+ * |  MISO   |D12         |
+ * |  MOSI   |D11         |
+ * |  DIO0   |D2          |
+ * |----------------------|
+ * |----------------------|
+ * | RFM95/96|ESP32       |
+ * |  NSS    |GPIO05      |
+ * |  SCK    |GPIO18      |
+ * |  MISO   |GPIO19      |
+ * |  MOSI   |GPIO23      |
+ * |  DIO0   |GPIO15      |
+*/
 
 #include <RHReliableDatagram.h>
 #include <RH_RF95.h>
@@ -13,11 +32,11 @@
 #define SERVER_ADDRESS 2
 
 // Singleton instance of the radio driver
-RH_RF95 driver;
-//RH_RF95 driver(5, 2); // Rocket Scream Mini Ultra Pro with the RFM95W
+RH_RF95 driver(10,2);  // 
+//RH_RF95 driver(5, 15);  //ESP32---> (RFM95_CS, RFM95_INT)
 
 // Class to manage message delivery and receipt, using the driver declared above
-RHReliableDatagram manager(driver, CLIENT_ADDRESS);
+RHReliableDatagram manager(driver, SERVER_ADDRESS);
 
 // Need this on Arduino Zero with SerialUSB port (eg RocketScream Mini Ultra Pro)
 //#define Serial SerialUSB
@@ -29,7 +48,7 @@ void setup()
 //  pinMode(4, OUTPUT);
 //  digitalWrite(4, HIGH);
 
-  Serial.begin(9600);
+  Serial.begin(115200);
   while (!Serial) ; // Wait for serial port to be available
   if (!manager.init())
     Serial.println("init failed");
@@ -38,7 +57,8 @@ void setup()
   // The default transmitter power is 13dBm, using PA_BOOST.
   // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
   // you can set transmitter powers from 5 to 23 dBm:
-//  driver.setTxPower(23, false);
+  driver.setTxPower(5, false);
+  driver.setFrequency(433.0);
   // If you are using Modtronix inAir4 or inAir9,or any other module which uses the
   // transmitter RFO pins and not the PA_BOOST pins
   // then you can configure the power transmitter power for -1 to 14 dBm and with useRFO true. 
@@ -50,34 +70,28 @@ void setup()
 //  driver.setCADTimeout(10000);
 }
 
-uint8_t data[] = "Hello World!";
+uint8_t data[] = "And hello back to you";
 // Dont put this on the stack:
 uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
 
 void loop()
 {
-  Serial.println("Sending to rf95_reliable_datagram_server");
-    
-  // Send a message to manager_server
-  if (manager.sendtoWait(data, sizeof(data), SERVER_ADDRESS))
+  if (manager.available())
   {
-    // Now wait for a reply from the server
+    // Wait for a message addressed to us from the client
     uint8_t len = sizeof(buf);
-    uint8_t from;   
-    if (manager.recvfromAckTimeout(buf, &len, 2000, &from))
+    uint8_t from;
+    if (manager.recvfromAck(buf, &len, &from))
     {
-      Serial.print("got reply from : 0x");
+      Serial.print("got request from : 0x");
       Serial.print(from, HEX);
       Serial.print(": ");
       Serial.println((char*)buf);
-    }
-    else
-    {
-      Serial.println("No reply, is rf95_reliable_datagram_server running?");
+
+      // Send a reply back to the originator client
+      if (!manager.sendtoWait(data, sizeof(data), from))
+        Serial.println("sendtoWait failed");
     }
   }
-  else
-    Serial.println("sendtoWait failed");
-  delay(500);
 }
 
